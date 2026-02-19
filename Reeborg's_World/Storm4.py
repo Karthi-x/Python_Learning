a3455e8c459dd5e81e195a4e6210742045366e73
Karthik Shaji <iamkarthikshaji@gmail.com>
2026-02-19 19:38:00 +0400

    Implement picking and moving functions in Reeborg's World Storm4.py

---
+++ Reeborg's_World/Storm4.py
@@ -0,0 +1,73 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def pick():
+    while object_here():
+        take()
+        
+def pickngo():
+    while not wall_in_front():
+        if front_is_clear():
+            move()
+            pick()
+        else:
+            deviate()
+        
+def countngo():
+    global rows
+    while not wall_in_front():
+        move()
+        pick()
+        rows+=1
+        
+def deviate():
+    turn_right()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_right()
+        
+def completepick():
+    pickngo()
+    if is_facing_north():
+        turn_left()
+        move()
+        pick()
+        turn_left()
+    else:
+        turn_right()
+        move()
+        pick()
+        turn_right()
+ 
+rows=1
+        
+countngo()
+turn_left()
+if rows%2==1:
+    while front_is_clear():
+        move()
+    turn_left()
+    turn_left()
+        
+for i in range(rows-1):
+    completepick()
+    i+=1
+    
+pickngo()
+while carries_object():
+    toss()
+    
+    
+turn_left()
+move()
+pick()
+turn_right()    
+pickngo()
+turn_right()    
+pickngo()
